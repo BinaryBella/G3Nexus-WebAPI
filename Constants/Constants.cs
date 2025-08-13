6a97f37b6733df057b92dbe4d8212e01428fe376
Chathushi Kavindya <chathushikavindya09@gmail.com>
2025-08-13 20:13:31 +0530

    feat: Remove unused Constants class to streamline codebase

--- Constants/Constants.cs
+++
@@ -1,9 +0,0 @@
-namespace G3NexusBackend.Constants;
-
-public class Constants
-{
-    public static string EmployeeAdmin = "EmployeeAdmin";
-    public static string Employee = "EmployeeUser";
-    public static string ClientAdmin = "ClientAdmin";
-    public static string ClientUser = "ClientUser";
-}
